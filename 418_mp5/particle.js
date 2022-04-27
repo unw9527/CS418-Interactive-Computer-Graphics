8ac92d515550587dac1653df7ca828c3b3119fb3
unw9527 <1041593558@qq.com>
2022-04-26 23:58:58 -0500

    fix velocity:

--- 418_mp5/particle.js
+++ 418_mp5/particle.js
@@ -15,20 +15,24 @@ var gravity = glMatrix.vec3.fromValues(0, -10, 0);
 /** @global box size */
 var boxsize = 2;
 
+/** @global normals of walls */
+var wallNormal = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]];
+
 class Particle{
     constructor(){
         this.velocity = glMatrix.vec3.create();
-        glMatrix.vec3.random(this.velocity, 10);
-        this.radius = Math.random() * (0.4 - 0.1) + 0.1; // random magic numbers
-        this.position = glMatrix.vec3.fromValues(randInitPos(-1, 1), randInitPos(-1, 1), randInitPos(-1, 1));
+        glMatrix.vec3.random(this.velocity, 10); // a random magic number
+        this.radius = randRange(0.15, 0.45); // random magic numbers
+        this.position = glMatrix.vec3.fromValues(randRange(-1, 1), randRange(-1, 1), randRange(-1, 1));
         this.color = glMatrix.vec3.fromValues(Math.random(), Math.random(), Math.random());
         this.stopMoving = false;
     }
 
     update(t){
         if (this.stopMoving) {return;}
-        let E = m * (-gravity[1]) * (this.position[1] - this.radius + boxsize) + 0.5 * m * Math.pow(this.velocity[1], 2);
-        if (E < 0.8){ // 0.8 is based on trial
+        let velSquare = glMatrix.vec3.dot(this.velocity, this.velocity);
+        let E = m * (-gravity[1]) * (this.position[1] - this.radius + boxsize) + 0.5 * m * velSquare;
+        if (E < 0.3){ // 0.3 is based on trial to make the animation physically appealing
             this.stopMoving = true;
             return;
         }
@@ -47,27 +51,47 @@ class Particle{
         // collision detection
         let delta_t = Infinity;
         let temp_t;
+        let idx = -1;
         for (let i = 0; i < 3; i++) {
             if (this.radius - newPos[i] > boxsize){
                 temp_t = ((this.position[i] - this.radius) + boxsize) / Math.abs(this.velocity[i]);
+                if (temp_t < delta_t){
+                    delta_t = temp_t;
+                    idx = i * 2;
+                }
             }
             if (this.radius + newPos[i] > boxsize){
                 temp_t = (boxsize - (this.position[i] + this.radius)) / Math.abs(this.velocity[i]);
-            }
-            if (temp_t < delta_t){
-                delta_t = temp_t;
-                this.velocity[i] *= -0.9;
+                if (temp_t < delta_t){
+                    delta_t = temp_t;
+                    idx = i * 2 + 1;
+                }
             }
         }
         if (delta_t != Infinity) {
             glMatrix.vec3.scale(newPos, this.velocity, delta_t);
             glMatrix.vec3.add(newPos, this.position, newPos);
             t -= delta_t;
+            
+            // v2 = v1 - 2 * (v1 · n) * n
+            let normals = wallNormal[idx];
+            let temp = glMatrix.vec3.dot(this.velocity, normals);
+            let b = glMatrix.vec3.fromValues(2 * temp, 2 * temp, 2 * temp);
+            glMatrix.vec3.multiply(b, b, normals);
+            glMatrix.vec3.subtract(this.velocity, this.velocity, b);
+            glMatrix.vec3.scale(this.velocity, this.velocity, 0.9);
         }
         glMatrix.vec3.copy(this.position, newPos);
     }
 }
 
-function randInitPos(min, max) { 
+
+/**
+ * Calculate a random starting position for each particle
+ * @param {number} min The minimum of the starting position
+ * @param {number} max The maximum of the starting position
+ */
+function randRange(min, max) { 
     return Math.random() * (max - min) + min;
 }
+
